792b354e4ba0f1956f7527d9e8131b6e86147990
alekhya <alekhyauma99@gmail.com>
2023-10-12 16:49:56 +0530

    ass4

--- see.js
+++ see.js
@@ -10,16 +10,29 @@ header.style['border-bottom'] = "3px solid black";
 var title= document.getElementById('header-title');
 title.style.fontWeight = 'bold';
 
-var li = document.querySelectorAll('li');
-console.log(li);
-console.log(li[1]);
-li[1].textcontent = 'hello2';
-li[1].style.fontWeight='bold';
-li[1].style.backgroundColor = 'yellow';
+//var li = document.querySelectorAll('li');
+//console.log(li);
+//console.log(li[1]);
+//li[1].textcontent = 'hello2';
+//li[1].style.fontWeight='bold';
+//li[1].style.backgroundColor = 'yellow';
 
 
 // Loop through the list items and apply styles
-for (var i = 0; i < li.length; i++) {
-  items[i].style.backgroundColor = '#f4f4f4';
+//for (var i = 0; i < li.length; i++) {
+  //items[i].style.backgroundColor = '#f4f4f4';
+//}
+var titles =document.querySelectorAll('.title');
+
+console.log(titles);
+titles[0].textContent = 'Hello';
+
+var secondItem = document.querySelectorAll('#items li')[1];
+secondItem.style.color = 'green';
+
+var oddItems = document.querySelectorAll('#items li:nth-child(odd)');
+for (var i = 0; i < oddItems.length; i++) {
+  oddItems[i].style.backgroundColor = 'green';
 }
+
 
