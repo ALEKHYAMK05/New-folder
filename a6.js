dc3c1dc2e21c7cc5e4771598e2e2eccebb72f84b
alekhya <alekhyauma99@gmail.com>
2023-10-19 16:46:16 +0530

    assi8

--- a6.js
+++ a6.js
@@ -2,6 +2,7 @@ var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
 var filter = document.getElementById('filter');
 
+// Load items from local storage when the page loads
 document.addEventListener('DOMContentLoaded', loadItems);
 
 // Form submit event
@@ -12,64 +13,74 @@ itemList.addEventListener('click', removeItem);
 filter.addEventListener('keyup', filterItems);
 
 // Add item
-function addItem(e){
+function addItem(e) {
   e.preventDefault();
 
   // Get input value
   var newItem = document.getElementById('item').value;
-  
+
   if (newItem !== '') {
-  // Create new li element
+    // Create new li element
     var li = document.createElement('li');
-  // Add class
+    // Add class
     li.className = 'list-group-item';
-  // Add text node with input value
+    // Add text node with input value
     li.appendChild(document.createTextNode(newItem));
 
-  // Create del button element
+    // Create del button element
     var deleteBtn = document.createElement('button');
 
-  // Add classes to del button
+    // Add classes to del button
     deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
 
-  // Append text node
+    // Append text node
     deleteBtn.appendChild(document.createTextNode('X'));
 
-  // Append button to li
+    // Append button to li
     li.appendChild(deleteBtn);
 
-  // Append li to list
+    // Append li to list
     itemList.appendChild(li);
+
+    // Store the item in local storage
+    storeItemInLocalStorage(newItem);
+
+    // Clear the input field
+    document.getElementById('item').value = '';
+  }
 }
-}
+
 // Remove item
-function removeItem(e){
-  if(e.target.classList.contains('delete')){
-    if(confirm('Are You Sure?')){
+function removeItem(e) {
+  if (e.target.classList.contains('delete')) {
+    if (confirm('Are You Sure?')) {
       var li = e.target.parentElement;
       itemList.removeChild(li);
+
+      // Remove the item from local storage
       removeItemFromLocalStorage(li);
     }
-    
   }
 }
 
 // Filter Items
-function filterItems(e){
+function filterItems(e) {
   // convert text to lowercase
   var text = e.target.value.toLowerCase();
   // Get lis
   var items = itemList.getElementsByTagName('li');
   // Convert to an array
-  Array.from(items).forEach(function(item){
+  Array.from(items).forEach(function (item) {
     var itemName = item.firstChild.textContent;
-    if (itemName.toLowerCase().indexOf(text) != -1){
+    if (itemName.toLowerCase().indexOf(text) !== -1) {
       item.style.display = 'block';
-    } else{
+    } else {
       item.style.display = 'none';
     }
   });
 }
+
+// Store item in local storage
 function storeItemInLocalStorage(item) {
   var items;
   if (localStorage.getItem('items') === null) {
