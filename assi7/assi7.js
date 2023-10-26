f4581e01368364248f07eb2ba0616c2e1a1cb743
alekhya <alekhyauma99@gmail.com>
2023-10-26 16:52:57 +0530

    assi9

--- assi7/assi7.js
+++ assi7/assi7.js
@@ -1,59 +1,78 @@
-
 const dataList = document.getElementById('dataList');
+let editIndex = -1; // To track the currently editing entry
 
-        // Check if there's existing data in local storage
-        const existingData = JSON.parse(localStorage.getItem('personData')) || [];
 
-        // Populate the data on the screen
-        function displayData() {
-            dataList.innerHTML = ''; // Clear the existing data
-            existingData.forEach((data, index) => {
-                const dataDiv = document.createElement('div');
-                dataDiv.innerHTML = `
-                    <p><strong>Name:</strong> ${data.name}</p>
-                    <p><strong>Email:</strong> ${data.email}</p>
-                    <p><strong>Phone Number:</strong> ${data.phone}</p>
-                    <button id="delete${index}">Delete</button>
-                `;
-                dataList.appendChild(dataDiv);
+const existingData = JSON.parse(localStorage.getItem('personData')) || [];
 
-                // Add click event listeners to the delete buttons
-                document.getElementById(`delete${index}`).addEventListener('click', () => {
-                    // Remove the data from both screen and storage
-                    existingData.splice(index, 1);
-                    localStorage.setItem('personData', JSON.stringify(existingData));
-                    displayData(); // Refresh the displayed data
-                });
-            });
-        }
 
-        // Display existing data (if any)
-        displayData();
+function displayData() {
+    dataList.innerHTML = ''; // Clear the existing data
+    existingData.forEach((data, index) => {
+        const dataDiv = document.createElement('div');
+        dataDiv.innerHTML = `
+            <p><strong>Name:</strong> <span id="name${index}">${data.name}</span></p>
+            <p><strong>Email:</strong> <span id="email${index}">${data.email}</span></p>
+            <p><strong>Phone Number:</strong> <span id="phone${index}">${data.phone}</span></p>
+            <button id="edit${index}">Edit</button>
+            <button id="delete${index}">Delete</button>
+        `;
+        dataList.appendChild(dataDiv);
 
-        document.getElementById('save').addEventListener('click', function() {
-            const name = document.getElementById('name').value;
-            const email = document.getElementById('email').value;
-            const phone = document.getElementById('phone').value;
-
-            // Create a new object with the data
-            const newData = {
-                name,
-                email,
-                phone
-            };
-
-            // Append the new data to the existing data
-            existingData.push(newData);
-
-            // Store the updated data in local storage
-            localStorage.setItem('personData', JSON.stringify(existingData));
-
-            // Clear the input fields
-            document.getElementById('name').value = '';
-            document.getElementById('email').value = '';
-            document.getElementById('phone').value = '';
-
-            // Refresh the displayed data
-            displayData();
+      
+        document.getElementById(`edit${index}`).addEventListener('click', () => {
+            editIndex = index;
+            // Populate the input fields with the data for editing
+            document.getElementById('name').value = data.name;
+            document.getElementById('email').value = data.email;
+            document.getElementById('phone').value = data.phone;
         });
-  
+
+        
+        document.getElementById(`delete${index}`).addEventListener('click', () => {
+            // Remove the data from both screen and storage
+            existingData.splice(index, 1);
+            localStorage.setItem('personData', JSON.stringify(existingData));
+            displayData(); // Refresh the displayed data
+        });
+    });
+}
+
+
+displayData();
+
+document.getElementById('save').addEventListener('click', function() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+
+    if (editIndex === -1) {
+   
+        const newData = {
+            name,
+            email,
+            phone
+        };
+
+       
+        existingData.push(newData);
+    } else {
+        // Edit existing data
+        existingData[editIndex] = {
+            name,
+            email,
+            phone
+        };
+        editIndex = -1; 
+    }
+
+    
+    localStorage.setItem('personData', JSON.stringify(existingData));
+
+    
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('phone').value = '';
+
+    
+    displayData();
+});
